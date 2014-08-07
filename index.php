a51e0863283d41d3238be3ecd6eecf5fa96d32e4
Ariel Vernaza <ariel.vernaza@admios-sa.com>
2014-08-07 05:47:00 -0500

    changinlogic

--- index.php
+++ index.php
@@ -27,10 +27,53 @@ if (strstr($rtmp_server, "://localhost/")) echo "<P class='warning'>Warning: You
 	</form></div>
 	
 <div class="info">
+  <p><b>Suggestions</b></p>
   <?php
-if (file_exists("uploads/last.html")) include("uploads/last.html");
-else echo "No snapshot found.";
+include("flash_detect.php")
 ?>
+ 
+<div class="container">
+
+<div class="page-header">
+    <h1>Whisper Stream<small> Pick the channel that you want to see</small></h1>
+</div>
+
+<!-- Responsive Gallery - START -->
+
+<style>
+img {
+    filter: gray; /* IE6-9 */
+    -webkit-filter: grayscale(1); /* Google Chrome, Safari 6+ & Opera 15+ */
+    -webkit-box-shadow: 0px 2px 6px 2px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 2px 6px 2px rgba(0,0,0,0.75);
+    box-shadow: 0px 2px 6px 2px rgba(0,0,0,0.75);
+    margin-bottom: 20px;
+}
+img:hover {
+    filter: none; /* IE6-9 */
+    -webkit-filter: grayscale(0); /* Google Chrome, Safari 6+ & Opera 15+ */
+}
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="text-center">
+            <h1>Model Gallery</h1>
+        </div>
+        <div class="row">
+     <?php
+        if (file_exists("uploads/last.html")){
+          include("uploads/last.html");
+        }  
+          ?>
+        </div>
+
+    </div>
+</div>
+
+</div> 
+
+
 </p>
 </div>
 <?php
